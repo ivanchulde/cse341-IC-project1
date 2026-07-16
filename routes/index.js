9f1f4095e446716b2eb4e0816f280ba7a9fe13ff
Ivan <aniv.ing.ec@gmail.com>
2026-07-15 22:48:35 -0500

    Add Swagger documentation4.0

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-
+// Idex Page
+// #swagger.tags = ['Index']
+// #swagger.description = 'Index page'
 router.get('/', (req, res) => {
   res.send('Hello, World!');
 });
